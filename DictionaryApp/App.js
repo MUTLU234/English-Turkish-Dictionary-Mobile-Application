0cc36155b07aa36718a215d861b7d96c4aa7204d
Nurettin Mutlu TÜVER <mutlutuver@gmail.com>
2024-05-03 02:55:37 +0300

    First commit(Create Project)

---
+++ DictionaryApp/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize:45,color:'red'}}>DICTIONARY APP</Text>
+      <Text style={{fontSize:25,color:'blue'}}>English-Turkish</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
