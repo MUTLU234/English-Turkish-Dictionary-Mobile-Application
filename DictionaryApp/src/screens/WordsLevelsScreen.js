7469f63fbe5e1c82a3443750b19e28aa4445db5d
Nurettin Mutlu TÜVER <mutlutuver@gmail.com>
2024-05-03 03:08:22 +0300

    Second commit(Add pages and functions)

---
+++ DictionaryApp/src/screens/WordsLevelsScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function WordsLevelsScreen() {
+  return (
+    <View>
+      <Text>WordsLevelsScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
